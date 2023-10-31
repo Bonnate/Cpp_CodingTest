93797aea3027e1b3ac30b4a92072111030018bce
PARKSEUNGGEUN <50767599+Bonnate@users.noreply.github.com>
2023-11-01 00:20:23 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 352.05 ms, Memory: 240 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.cpp
@@ -7,13 +7,17 @@ using namespace std;
 vector<int> solution(vector<int> numbers) {
 	vector<int> answer(numbers.size());
 
-	stack<pair<int, int>> s;
+	
+	stack<pair<int, int>> s; // 현재 값, 인덱스 번호를 가지는 스택
 	for (int i = 0; i < numbers.size(); ++i)
 	{
+		// 각 숫자마다 while문을 이용하여 반복
 		while (!s.empty())
 		{
+			// 현재 숫자가 s.top의 숫자보다 크다면?
 			if (numbers[i] > s.top().first)
 			{
+				// 가장 가까이에 있는 큰 숫자
 				answer[s.top().second] = numbers[i];
 				s.pop();
 			}
@@ -24,8 +28,10 @@ vector<int> solution(vector<int> numbers) {
 		s.push({ numbers[i], i });
 	}
 
+	// 스택에서 빠지지 않은 값들은 큰 수가 없는 경우
 	while (!s.empty())
 	{
+		// 해당 위치에 -1로 설정
 		answer[s.top().second] = -1;
 		s.pop();
 	}
