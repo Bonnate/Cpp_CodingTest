2d8617afe6a4342c9cee934c9d0b14a3d65bb58f
PARKSEUNGGEUN <50767599+Bonnate@users.noreply.github.com>
2023-11-18 22:59:56 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.cpp
@@ -7,30 +7,30 @@ using namespace std;
 
 int solution(int bridge_length, int weight, vector<int> truck_weights) {
     queue<pair<int, int>> q;
-    int sum = 0;
-    int idx = 0;
-    int time = 0;
-    int n = truck_weights.size();
+    int sum = 0; // 현재 다리에 있는 트럭의 무게 합
+    int idx = 0; // 삽입할 인덱스 번호
+    int time = 0; // 경과된 시간
+    int n = truck_weights.size(); // 트럭의 개수
     
     while(idx < n)
     {
+        // q가 비어있지 않으면서, q의 front의 시간이 경과되었다면? (도착)
         if(!q.empty() && time >= q.front().second)
         {
-            sum -= q.front().first;
-            int answer = q.front().second;
-            q.pop();
+            sum -= q.front().first; // 무게에서 빼기
+            q.pop(); // 큐에서 제거
         }
         
+        // idx번째의 트럭을 다리에 둘 수 있는가? (무게를 초과하지 않는가?)
         if(sum + truck_weights[idx] <= weight)
         {
-            q.push({truck_weights[idx], time + bridge_length});
-            sum += truck_weights[idx++];
+            q.push({truck_weights[idx], time + bridge_length}); // 큐에 트럭의 무게와 도착 예정 시간 삽입
+            sum += truck_weights[idx++]; // 무게 합
         }
         
-        ++time;
+        ++time; // 시간 경과
     }
     
-    while(q.size() != 1)
-        q.pop();
-    return q.front().second + 1;
+    // 큐의 가장 마지막 원소의 시간 + 1을 리턴
+    return q.back().second + 1;
 }
