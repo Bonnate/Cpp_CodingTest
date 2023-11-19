601540df8d69e723a8932b772e67d205528bb015
PARKSEUNGGEUN <50767599+Bonnate@users.noreply.github.com>
2023-11-19 21:42:16 +0900

    [level 2] Title: 배달, Time: 0.03 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,47 +1,54 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <climits>
 using namespace std;
 
 // 간선 정보
 struct Edge {
-    int from, to, weight;
+    int to, weight;
 };
 
 int solution(int N, vector<vector<int>> road, int K) {
-    vector<Edge> edges;
+    vector<vector<Edge>> graph(N + 1);
     vector<int> dist(N + 1, INT_MAX);
 
-    // 그래프 정보 초기화 (무방향 그래프로 양측 노드에 삽입)
+    // 그래프 정보 초기화 (양방향)
     for (vector<int> r : road) {
-        edges.push_back({ r[0], r[1], r[2] });
-        edges.push_back({ r[1], r[0], r[2] });
+        int from = r[0];
+        int to = r[1];
+        int weight = r[2];
+        graph[from].push_back({to, weight});
+        graph[to].push_back({from, weight});
     }
 
-    // 시작 노드의 거리는 0
-    dist[1] = 0; 
+    // 우선순위 큐를 이용한 다익스트라 알고리즘
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
 
-    // 벨만-포드 알고리즘
-    // 모든 노드에 대한 가장 짧은 경로는 노드의 수보다 하나 작은 간선의 수(N-1)로 이루어지기에 N-1번 반복
-    for (int i = 0; i < N - 1; ++i)
-        for (Edge edge : edges) 
-        {
-            int from = edge.from;
-            int to = edge.to;
-            int weight = edge.weight;
-    
-            // 아직 다른 간선에 의해 비용이 계산되지 않은경우에는 무시
-            if(dist[from] == INT_MAX)
-                continue;
-            
-            // 현재 위치의 비용(dist[from]) + 다음 위치까지의 비용(weight)이 저장된 비용(dist[to])보다 작으면 갱신
-            dist[to] = min(dist[to], dist[from] + weight);
+    dist[1] = 0; // 시작 노드의 거리는 0
+    pq.push({0, 1}); // {거리, 노드} 쌍을 우선순위 큐에 삽입
+
+    while (!pq.empty()) {
+        int current_distance = pq.top().first;
+        int current_node = pq.top().second;
+        pq.pop();
+
+        // 현재 노드에서 갈 수 있는 모든 노드들에 대해 최단 거리 갱신
+        for (Edge& e : graph[current_node]) {
+            int next_node = e.to;
+            int weight = e.weight;
+
+            if (dist[next_node] > current_distance + weight) {
+                dist[next_node] = current_distance + weight;
+                pq.push({dist[next_node], next_node});
+            }
         }
-    
+    }
+
     // 정답 구하기
     int answer = 0;
-    for(int i = 1; i <= N; ++i)
-        if(dist[i] <= K)
+    for (int i = 1; i <= N; ++i)
+        if (dist[i] <= K)
             ++answer;
 
     return answer;
