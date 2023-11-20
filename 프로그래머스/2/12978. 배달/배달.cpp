32d20b44387bdeecd38f7e433a62231d6a585259
PARKSEUNGGEUN <50767599+Bonnate@users.noreply.github.com>
2023-11-20 09:14:43 +0900

    [level 2] Title: 배달, Time: 0.10 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.cpp
+++ 프로그래머스/2/12978. 배달/배달.cpp
@@ -1,68 +1,47 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <climits>
+#include <algorithm>
 using namespace std;
 
-// 간선 정보
-struct Edge2 {
-	int to, weight;
+struct Edge
+{
+    int from, to, weight;
 };
 
-int solution(int N, vector<vector<int>> road, int K) {
-	vector<vector<Edge2>> graph(N + 1);
-	vector<int> dist(N + 1, INT_MAX);
+int solution(int N, vector<vector<int> > road, int K) {
+    int answer = 0;
 
-	// 그래프 정보 초기화 (양방향)
-	for (vector<int> r : road) {
-		int from = r[0];
-		int to = r[1];
-		int weight = r[2];
-		graph[from].push_back({ to, weight });
-		graph[to].push_back({ from, weight });
-	}
+    vector<Edge> edges;
+    for(vector<int> v : road)
+    {
+        edges.push_back({v[0], v[1], v[2]});
+        edges.push_back({v[1], v[0], v[2]});
+    }
+    
+    vector<int> dist(N + 1, INT_MAX);
+    dist[1] = 0;
+    for(int i = 0; i < N - 1; ++i)
+        for(Edge edge : edges)
+        {
+            int from = edge.from;
+            int to = edge.to;
+            int weight = edge.weight;
 
-	// 우선순위 큐를 이용한 다익스트라 알고리즘
-	priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+            if(dist[from] == INT_MAX)
+                continue;
 
-	dist[1] = 0; // 시작 노드의 거리는 0
-	pq.push({ 0, 1 }); // {거리, 노드} 쌍을 우선순위 큐에 삽입
-
-	while (!pq.empty()) {
-		int current_distance = pq.top().first;
-		int current_node = pq.top().second;
-		pq.pop();
-
-		// printf("현재위치: %d, 누적비용: %d\n", current_node, current_distance);
-
-		// 현재 노드에서 갈 수 있는 모든 노드들에 대해 최단 거리 갱신
-		for (Edge2& e : graph[current_node])
-		{
-			int next_node = e.to;
-			int weight = e.weight;
-
-			// printf("다음 노드: %d, 비용: %d\n", next_node, weight);
-
-			if (dist[next_node] > current_distance + weight)
-			{
-				dist[next_node] = current_distance + weight;
-				pq.push({ dist[next_node], next_node });
-
-				// printf("노드 갱신: %d, 비용: %d\n", next_node, dist[next_node]);
-			}
-		}
-	}
-
-	// 정답 구하기
-	int answer = 0;
-	for (int i = 1; i <= N; ++i)
-	{
-		if (dist[i] <= K)
-			++answer;
-
-		cout << dist[i] << " ";
-	}
-
-
-	return answer;
+            dist[to] = min(dist[to], dist[from] + weight);
+        }
+    
+    for(int i = 1; i <= N; ++i)
+    {
+        cout<<dist[i]<<endl;
+    }
+    
+    for(int i = 1; i <= N; ++i)
+        if(dist[i] <= K)
+            ++answer;
+    
+    return answer;
 }
