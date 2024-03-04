3c5df96cc8fc641917f9739205d5235bd2d819fa
PARKSEUNGGEUN <50767599+Bonnate@users.noreply.github.com>
2024-03-04 12:51:17 +0900

    [Silver V] Title: 날짜 계산, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1476. 날짜 계산/날짜 계산.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int year = 1;
+    
+    int E, S, M;
+    cin >> E >> S >> M;
+    
+    int e = 1, s = 1, m = 1;
+
+    for(int i = 1; i <= 7980; ++i)
+    {
+        if(e == E && s == S && m == M)
+        {
+            printf("%d", i);
+            break;
+        }
+        
+        ++e;
+        ++s;
+        ++m;
+        
+        if(e == 16)
+            e = 1;
+        if(s == 29)
+            s = 1;
+        if(m == 20)
+            m = 1;
+    }
+
+    return 0;
+}
